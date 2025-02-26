cd3f9f95e6ba8a4ce5f3b7015d91226aff004d4b
DSHarmanSingh <harmannamrah69@gmail.com>
2025-02-26 21:20:44 +0530

    Update DiseasePredictionDL.py

--- DiseasePredictionDL.py
+++ DiseasePredictionDL.py
@@ -1,31 +1,3 @@
-import numpy as np
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-df= pd.read_csv(r"/kaggle/input/diseaseandsymptoms/DiseaseAndSymptoms.csv")
-#Standardizing symptoms names by removing spaces with underscore, and lower casing,
-df.columns= df.columns.str.strip().str.lower().str.replace(' ', '_')
-for col in df.columns:
-    df[col]= df[col].astype(str).str.strip().str.lower().str.replace(' ', '_')
-    
-#Handling Missing values
-#Droping columns of symptoms which have too many missing values
-threshold= len(df)* 0.1#Drop if more than 90% missing 
-df= df.dropna(thresh= threshold, axis=1)
-#Now filling the missing values with "no_symtoms"
-df.fillna("no_symptom", inplace= True)
-#Converting symptoms into lists per disease
-symptom_columns= [col for col in df.columns if "symptom" in col]#for identifying symptoms columsn
-df["symptoms_list"]= df[symptom_columns].apply(lambda row: " ".join(set(row.values) - {"no_symptom"}), axis=1)
-#drop all odl symptom columns
-df= df[["disease", "symptoms_list"]]
-df.to_csv(r"/kaggle/working/diseaseSymptomsCleaned.csv", index=False)
-tfidf= TfidfVectorizer(max_features= 500)#Limiting features for efficiency
-X_tfidf= tfidf.fit_transform(df["symptoms_list"])
-tfidf_df= pd.DataFrame(X_tfidf.toarray(), columns= tfidf.get_feature_names_out())
-df_final= pd.concat([df[["disease"]], tfidf_df], axis= 1)
-df_final.to_csv(r"/kaggle/working/diseaseSymptomsVectorized.csv", index=False)
-
-
 import pandas as pd
 import numpy as np 
 import tensorflow as tf
@@ -36,7 +8,7 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 
-df_v= pd.read_csv(r"/kaggle/input/diseaseprecaution/Disease precaution.csv")
+df_v= pd.read_csv(r"C:\Users\harma\OneDrive\Desktop\datasets\diseaseSymptomsVectorized.csv")
 X= df_v.drop(columns= ["disease"])
 y= df_v["disease"]
 #Encode target variable
